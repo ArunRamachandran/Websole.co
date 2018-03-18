bbc5d710b959c2933a9eb128b3eac90b2a704eed
ArunRamachandran <myself.arunkr@gmail.com>
2018-03-18 12:13:20 +0530

    Added changes

--- app/components/NavbarIconSet.jsx
+++ app/components/NavbarIconSet.jsx
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import render from 'react-dom';
 
+import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
 import '../sass/navbariconset.scss'
@@ -24,6 +25,11 @@ export default class NavbarIconSet extends Component {
       <div className="navbar-icons">
         <RaisedButton label="FAQ" primary={true} style={style} />
         <RaisedButton label="Sign In" primary={true} style={style} />
+        <IconButton
+          iconClassName="muidocs-icon-custom-github" tooltip="View code"
+          tooltipPosition="bottom-center"
+        />
+
         {/*<RaisedButton
           href="https://github.com/callemall/material-ui"
           target="_blank"
